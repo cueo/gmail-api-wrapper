28dec848e6dde21c2922be0d7d542f6143f44379
cueo <mohit.ritanil@gmail.com>
2020-08-09 22:38:08 +0530

    Add main class for some use-cases

---
+++ main.py
@@ -0,0 +1,20 @@
+from data import BASIC_SETTINGS_SCOPE, LABEL_SCOPE, TRANSACTION_SENDERS
+from filter import Filter
+from pprint import pprint
+
+from label import Label
+
+# If modifying these scopes, delete the file token.pickle.
+from setup import authorize
+
+SCOPES = [BASIC_SETTINGS_SCOPE, LABEL_SCOPE]
+
+if __name__ == '__main__':
+    service = authorize(SCOPES)
+    label = Label(service)
+
+    label.create_label('Transactions')
+    pprint(label.all_labels())
+
+    _filter = Filter(service)
+    _filter.create_filter(remove_labels=['INBOX'], add_labels=['Transactions'], senders=TRANSACTION_SENDERS)

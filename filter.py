3182b1ee84367afc96c8537b2190315e56e4849d
cueo <mohit.ritanil@gmail.com>
2020-08-09 22:37:33 +0530

    Add Filter class
    Create new or update existing filters

---
+++ filter.py
@@ -0,0 +1,61 @@
+from label import Label
+
+
+class Filter:
+    def __init__(self, service):
+        self.service = service
+        self.label_service = Label(service)
+
+    def create_filter(self, add_labels, remove_labels, senders):
+        labels = add_labels + remove_labels
+        label_ids = self.label_service.get_label_ids(labels)
+        print('Label and label ids:', label_ids)
+        from_emails = ' OR '.join(senders)
+        add_label_ids = [label_ids[label_id] for label_id in add_labels]
+        remove_label_ids = [label_ids[label_id] for label_id in remove_labels]
+        print('Setting filter for from=%s add_labels=%s remove_labels=%s'
+              % (from_emails, add_label_ids, remove_label_ids))
+
+        new_filter = {
+            'criteria': {
+                'from': from_emails
+            },
+            'action': {
+                'addLabelIds': add_label_ids,
+                'removeLabelIds': remove_label_ids
+            }
+        }
+        result = self.service.users().settings().filters().create(userId='me', body=new_filter).execute()
+        print('Created filter: %s' % result.get('id'))
+
+    def update_filter(self, label, sender):
+        """
+        Update filter to add sender to the filter for the given label.
+        Args:
+            label: label to add for the sender's mails
+            sender: from address
+        """
+        label_id = self.label_service.get_label_id(label)
+        print('Label id: %s' % label_id)
+        filter_setting = self.get_filter(label_id)
+        if filter_setting is None:
+            print('Filter not found for the label: %s' % label)
+            return
+        senders = filter_setting['criteria']['from']
+        if sender in senders:
+            print('Filter already contains %s' % sender)
+            return
+        senders += ' OR %s' % sender
+        filter_setting['criteria']['from'] = senders
+        result = self.service.users().settings().filters().delete(userId='me', id=filter_setting['id']).execute()
+        print('Deleted filter: ', result)
+        result = self.service.users().settings().filters().create(userId='me', body=filter_setting)
+        print('Created filter: %s' % result.get('id'))
+
+    def get_filter(self, label_id):
+        filters = self.service.users().settings().filters().list(userId='me').execute()['filter']
+        # next(_filter for _filter in filters
+        #   if 'addLabelIds' in _filter['action'] and _filter['action']['addLabelIds'] == label_id)
+        for _filter in filters:
+            if label_id in _filter.get('action', {}).get('addLabelIds', []):
+                return _filter

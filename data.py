3f80ad0cf28d287df485fa62cbe86ae9dc8e12eb
cueo <mohit.ritanil@gmail.com>
2020-08-09 22:35:02 +0530

    Seed data and scopes

---
+++ data.py
@@ -0,0 +1,32 @@
+READONLY_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly'
+BASIC_SETTINGS_SCOPE = 'https://www.googleapis.com/auth/gmail.settings.basic'
+LABEL_SCOPE = 'https://www.googleapis.com/auth/gmail.labels'
+
+TRANSACTION_SENDERS = [
+    'Sbimfreports@camsonline.com',
+    'enq_D@camsonline.com',
+    'donotreply@camsonline.com',
+    'enq_g@camsonline.com',
+    'adityabirlacapital.mf@camsonline.com',
+    'Sony@email.sonyentertainmentnetwork.com',
+    'estatement@icicibank.com',
+    'alert@icicibank.com',
+    'net.statement@citicorp.com',
+    'CitiAlert.India@citicorp.com',
+    'noreply@swiggy.in',
+    'nse_alerts@nse.co.in',
+    'noreply@makemytrip.com',
+    'services@cdslindia.co.in',
+    'axismf@kfintech.online',
+    'no-reply@alerts.actcorp.in',
+    'no-reply@mailer5.actcorp.in',
+    'noreply@phonepe.com',
+    'noreply@lazypay.in',
+    'noreply@coinmailer.zerodha.net',
+    'noreply-cashier@qmailer.zerodha.net',
+    'no-reply@amazon.com',
+    'info@bseindia.in',
+    ' CRA@nsdl.co.in',
+    'cra-alerts@nsdl.co.in',
+    'order-update@amazon.in'
+]
